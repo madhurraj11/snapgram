4a4d66ceef6b83f91239493e547fbc4fa66cc86e
Madhur Chauhan <madhurchauhan1100@gmail.com>
2025-01-01 21:51:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ 
+      filename: "bundle-analysis.html", 
+      open: true, // Automatically opens the report in the browser
+      gzipSize: true, 
+      brotliSize: true 
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Separate libraries from node_modules into individual chunks
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Suppress chunk size warnings for up to 1MB
+  },
+});
